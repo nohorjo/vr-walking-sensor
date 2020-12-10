a5259774fc26f20bca0a1e98679842a3d6935375
Muhammed Haque <nohorjo@gmail.com>
2020-12-10 11:24:23 +0000

    Create blocking button handler

--- button.py
+++ button.py
@@ -1,6 +1,13 @@
 import RPi.GPIO as GPIO
 
 def create(pin, callback):
-    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
     GPIO.add_event_detect(pin, GPIO.RISING, callback = lambda x: callback())
 
+class Button:
+    def __init__(self, pin):
+        self.pin = pin
+        GPIO.setup(pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+    def isPressed(self):
+        return GPIO.input(self.pin)
--- test.py
+++ test.py
@@ -1,46 +1,24 @@
 import display
-import button
+from button import Button
 from motor import Motor
 
 from time import sleep
 from mpu6050 import mpu6050
 import RPi.GPIO as GPIO
 
-sensor = mpu6050(0x68)
-m = Motor([22, 23, 24, 25])
-
-angles = [10, 20, 30, 45, 60, 90, 120, 180, 240, 275, 360]
-selectedAngle = 0
-display.text('Will rotate %d' % angles[selectedAngle], 0)
-
-printAcc = True
-
-def button_callback1():
-    global printAcc, angles, selectedAngle
-    printAcc = not printAcc
-    display.text('Rotate anticlockwise', 1)
-    display.show()
-    m.rotate(-angles[selectedAngle])
-
-def button_callback2():
-    global printAcc, angles, selectedAngle
-    printAcc = not printAcc
-    selectedAngle = (selectedAngle + 1) % len(angles)
-    display.text('Will rotate %d' % angles[selectedAngle], 0)
-    display.show()
-
-def button_callback3():
-    global printAcc, angles, selectedAngle
-    display.text('Rotate clockwise', 1)
-    printAcc = not printAcc
-    display.show()
-    m.rotate(angles[selectedAngle])
-
-button.create(17, button_callback1)
-button.create(18, button_callback2)
-button.create(27, button_callback3)
-
 try:
+    sensor = mpu6050(0x68)
+    m = Motor([22, 23, 24, 25])
+    buttonLeft = Button(17)
+    buttonCenter = Button(18)
+    buttonRight = Button(27)
+
+    angles = [10, 20, 30, 45, 60, 90, 120, 180, 240, 275, 360]
+    selectedAngle = 0
+    display.text('Will rotate %d' % angles[selectedAngle], 0)
+
+    printAcc = True
+
     while True:
         accelerometer_data = sensor.get_accel_data()
         gyro_data = sensor.get_gyro_data()
@@ -56,9 +34,22 @@ try:
             display.text('Rot y: %f' % gyro_data['y'], 3)
             display.text('Rot z: %f' % gyro_data['z'], 4)
 
+        if buttonLeft.isPressed():
+            display.text('Rotate anticlockwise', 1)
+            m.rotate(-angles[selectedAngle])
+
+        if buttonCenter.isPressed():
+            printAcc = not printAcc
+            selectedAngle = (selectedAngle + 1) % len(angles)
+            display.text('Will rotate %d' % angles[selectedAngle], 0)
+
+        if buttonRight.isPressed():
+            display.text('Rotate clockwise', 1)
+            m.rotate(angles[selectedAngle])
+
         display.show()
 
-        sleep(0.25)
+        sleep(0.16)
 except:
     display.clear()
     GPIO.cleanup()
