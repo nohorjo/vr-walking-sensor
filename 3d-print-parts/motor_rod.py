44e5a0086505736326722d472675a23e8cde42e6
Muhammed Haque <nohorjo@gmail.com>
2020-12-31 16:48:11 +0000

    Create controller parts

---
+++ 3d-print-parts/motor_rod.py
@@ -0,0 +1,17 @@
+from solid import *
+from solid.utils import *
+
+from constants import *
+from moveable_point import *
+from super_hole import *
+
+if __name__ == '__main__':
+    x = 25.0
+    y = 7.0
+    z = 5.0
+    model = cube([x, y, z])
+    y2 = 3.2
+    model -= translate([2, (y - y2) / 2])(cube([5, y2, 10]))
+    model -= translate([x - 3, 0, z / 2])(rotate(90, LEFT_VEC)(cylinder(d = cable_d, h = 10)))
+    scad_render_to_file(model, '_%s.scad'% __file__[:-3])
+
---
+++ 3d-print-parts/top_tube_block.py
@@ -0,0 +1,16 @@
+from solid import *
+from solid.utils import *
+
+from constants import *
+from moveable_point import *
+from super_hole import *
+
+if __name__ == '__main__':
+    x = 13.0
+    y = 8.0
+    p = MoveablePoint()
+    model = cube([x, y, 3])
+    model += cube([5.5, y, 7])
+    model -= translate([(x / 2) + 2, y / 2])(cylinder(d = tube_d, h = 10))
+    scad_render_to_file(model, '_%s.scad'% __file__[:-3])
+
